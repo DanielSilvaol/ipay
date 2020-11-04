b6f2c5a776cef1ce768431ea591ba555d7863e2e
Henrique <henriquecidrin96@gmail.com>
2020-11-04 12:28:08 -0300

    ajuste no calculo de recomendacao, aberto algumas rotas para dados de compania

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from decimal import Decimal
-
-from flask import Flask, make_response
+from flask import Flask, make_response, json
 
 from plan_service import PlanService
 from sales_repository import SalesRepository
@@ -20,6 +18,20 @@ def predict_plan_product(product_name, price):
     price = float(price.replace(',', '.'))
     deal_response = plan_service.rate_deal(product_name, price)
     return make_response(deal_response, 200)
+
+
+@app.route("/plan/companies", methods=['GET'])
+def search_companies():
+    companies = plan_service.search_companies()
+    response = json.dumps(companies)
+    return make_response(response, 200)
+
+
+@app.route("/plan/classification/<classification>/companies", methods=['GET'])
+def search_companies_by_classification(classification):
+    companies = plan_service.search_companies_by_classification(classification)
+    response = json.dumps(companies)
+    return make_response(response, 200)
 
 
 if __name__ == '__main__':
--- sales_repository.py
+++ sales_repository.py
@@ -1,9 +1,14 @@
+import os
+
 from pymongo import MongoClient
 
 
 class SalesRepository:
 
-    __cluster = MongoClient("")
+    __mongo_uri = os.getenv('MONGODB_URI')
+    print(__mongo_uri)
+
+    __cluster = MongoClient("mongodb+srv://admin:loba@loba.jbtnz.mongodb.net/loba?retryWrites=true&w=majority")
     __db = __cluster["loba"]
     __sale_collection = __db["sale"]
 
@@ -11,6 +16,11 @@ class SalesRepository:
         return self.__sale_collection.find()
 
     def find_by_product_name(self, product_name):
-        return self.__sale_collection.find({'name': product_name})
+        return self.__sale_collection.find({"name": {"$regex": product_name, "$options": "i"}})
 
+    def find_all_company(self):
+        return self.__sale_collection.distinct('company')
+
+    def find_plans_by_classification(self, classification):
+        return self.__sale_collection.find({"classification": {"$regex": classification, "$options": "i"}})
 
