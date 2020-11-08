7e92fe6e538ff9452beaf0c11fb88c89005db7be
Daniel Silva <DanielSilvaol@outlook.com>
2020-11-08 15:30:45 -0300

    Commit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-import os
-from flask import Flask, jsonify, request
+from flask import Flask, make_response, json
 
+from plan_service import PlanService
+from sales_repository import SalesRepository
 
 app = Flask(__name__)
+sales_repository = SalesRepository()
+plan_service = PlanService(sales_repository)
 
 
 @app.route('/')
@@ -10,6 +13,26 @@ def index():
     return "Hello, It's IPay API!"
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+@app.route("/plan/product/<product_name>/price/<price>", methods=['GET'])
+def predict_plan_product(product_name, price):
+    price = float(price.replace(',', '.'))
+    deal_response = plan_service.rate_deal(product_name, price)
+    return make_response(deal_response, 200)
+
+
+@app.route("/plan/companies", methods=['GET'])
+def search_companies():
+    companies = plan_service.search_companies()
+    response = json.dumps(companies)
+    return make_response(response, 200)
+
+
+@app.route("/plan/classification/<classification>/companies", methods=['GET'])
+def search_companies_by_classification(classification):
+    companies = plan_service.search_companies_by_classification(classification)
+    response = json.dumps(companies)
+    return make_response(response, 200)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ matcher.py
@@ -0,0 +1,93 @@
+from sales_repository import SalesRepository
+
+
+class Matcher:
+
+    def __init__(self, sales_repository):
+        self._salesRepository = sales_repository
+        self._companies = sales_repository.find_all_company()
+
+    def match(self, search, dataset, rate_accept):
+        search = self._prepare(self._remove_company(search))
+        match_accepted = []
+        for data in dataset:
+            if self._match(search, data, rate_accept):
+                print(data)
+                match_accepted.append(data)
+        return match_accepted
+
+    def _match(self, search, data, rate_accept):
+
+        delta = 0
+        value = data['name'].lower()
+        delta += self._matcher_by_letter(search, self._prepare(value))
+        delta += self._matcher_by_word(search, value) * 3
+        delta += self._matcher_by_number(search, value) * 4
+
+        perfect_delta = search.__len__() + (search.split(" ").__len__() * 3) + (''.join([n for n in search if n.isdigit()]).__len__() * 4)
+        error_bound = perfect_delta * 0.2
+
+        ideal_delta = perfect_delta - error_bound
+        return delta / ideal_delta > rate_accept
+
+    @staticmethod
+    def _matcher_by_letter(search, value):
+        delta = 0
+        try:
+            for pos, letter in enumerate(search):
+                if value.find(letter, pos - 1, pos + 1) != -1:
+                    delta += 1
+                if value.find(letter) != -1:
+                    delta += 1
+        except Exception as exc:
+            print(f'matcher_by_letter {exc}')
+        return delta
+
+    @staticmethod
+    def _matcher_by_word(search, value):
+        delta = 0
+        try:
+            for word in value.split(' '):
+                if search.lower().find(word.lower()) != -1:
+                    delta += 1
+        except Exception as exc:
+            print(f"matcher_by_word {exc}")
+
+        return delta
+
+    @staticmethod
+    def _matcher_by_number(search, value):
+        delta = 0
+        try:
+            search_number = ''.join([n for n in search if n.isdigit()])
+            value = ''.join([n for n in value if n.isdigit()])
+            for letter in search_number:
+                pos = value.find(letter)
+                if pos != -1:
+                    value = value[:pos] + value[pos+1:]
+                    delta += 1
+        except Exception as exc:
+            print(f'matcher_by_number {exc}')
+        return delta
+
+    def _remove_company(self, value):
+        for company in self._companies:
+            value = value.lower().replace(company.lower(), '')
+        return value
+
+    @staticmethod
+    def _prepare(value):
+        return value.lower().strip().replace(' ', '')
+
+
+if __name__ == '__main__':
+
+    search = "20gb"
+    values = \
+        [
+         {"name": "13GB + Controle"},
+         {"name": "20GB Pré Pago"},
+         {"name": "8GB"},
+         {"name": "11GB"}]
+
+    print(Matcher(SalesRepository()).match(search, values, 0.7))
